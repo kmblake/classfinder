f0df5eff73651f21137a5f9eb5cdab05edb864b0
kmblake <kentblake@DN0a22b89a.SUNet>
2016-04-16 18:22:29 -0700

    Cleaned up code

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Define a new module for our app
-var app = angular.module("classfinder", ['uiGmapgoogle-maps']).config(function($sceDelegateProvider) {
+var app = angular.module("classfinder", []).config(function($sceDelegateProvider) {
   $sceDelegateProvider.resourceUrlWhitelist([
     // Allow same origin resource loads.
     'self',
@@ -53,7 +53,7 @@ app.controller('ClassfinderController', ['$scope', function($scope) {
 		{ 
 			title: 'CS 110',
 			desc:'Principles of Computer Systems',
-			bldg: 'CEMEX',
+			bldg: 'NVIDIA',
 			loc: "https://www.google.com/maps/embed/v1/place?q=37%C2%B025'41.2%22N%20122%C2%B010'26.7%22W&key=AIzaSyAOpii1ESYwlE64cPlCCXRijnkymgNJp4A"
 		}, 
 		{ 
@@ -106,76 +106,3 @@ app.controller('ClassfinderController', ['$scope', function($scope) {
 		}
 	];
 }])
-
-	// $scope.map = { center: { latitude: 45, longitude: -73 }, zoom: 8 };
-	// $scope.map = {center: {latitude: 40.1451, longitude: -99.6680 }, zoom: 4 };
-  // $scope.map = {center: {latitude: 37.4280888, longitude: -122.1723306 }, zoom: 16 };
-  // $scope.options = {scrollwheel: false};
-  // // $scope.coordsUpdates = 0;
-  // // $scope.dynamicMoveCtr = 0;
-  // $scope.marker = {
-  //   options: { draggable: false }
-  // };
-// }]);
-
-// function ClassfinderController($scope){
-
-// 	// The data model. These items would normally be requested via AJAX,
-// 	// but are hardcoded here for simplicity. See the next example for
-// 	// tips on using AJAX.
-
-// 	// $scope.items = [
-// 	// 	{
-// 	// 		url: 'http://tutorialzine.com/2013/07/50-must-have-plugins-for-extending-twitter-bootstrap/',
-// 	// 		title: '50 Must-have plugins for extending Twitter Bootstrap',
-// 	// 		image: 'http://cdn.tutorialzine.com/wp-content/uploads/2013/07/featured_4-100x100.jpg'
-// 	// 	},
-// 	// 	{
-// 	// 		url: 'http://tutorialzine.com/2013/08/simple-registration-system-php-mysql/',
-// 	// 		title: 'Making a Super Simple Registration System With PHP and MySQL',
-// 	// 		image: 'http://cdn.tutorialzine.com/wp-content/uploads/2013/08/simple_registration_system-100x100.jpg'
-// 	// 	},
-// 	// 	{
-// 	// 		url: 'http://tutorialzine.com/2013/08/slideout-footer-css/',
-// 	// 		title: 'Create a slide-out footer with this neat z-index trick',
-// 	// 		image: 'http://cdn.tutorialzine.com/wp-content/uploads/2013/08/slide-out-footer-100x100.jpg'
-// 	// 	},
-// 	// 	{
-// 	// 		url: 'http://tutorialzine.com/2013/06/digital-clock/',
-// 	// 		title: 'How to Make a Digital Clock with jQuery and CSS3',
-// 	// 		image: 'http://cdn.tutorialzine.com/wp-content/uploads/2013/06/digital_clock-100x100.jpg'
-// 	// 	},
-// 	// 	{
-// 	// 		url: 'http://tutorialzine.com/2013/05/diagonal-fade-gallery/',
-// 	// 		title: 'Smooth Diagonal Fade Gallery with CSS3 Transitions',
-// 	// 		image: 'http://cdn.tutorialzine.com/wp-content/uploads/2013/05/featured-100x100.jpg'
-// 	// 	},
-// 	// 	{
-// 	// 		url: 'http://tutorialzine.com/2013/05/mini-ajax-file-upload-form/',
-// 	// 		title: 'Mini AJAX File Upload Form',
-// 	// 		image: 'http://cdn.tutorialzine.com/wp-content/uploads/2013/05/ajax-file-upload-form-100x100.jpg'
-// 	// 	},
-// 	// 	{
-// 	// 		url: 'http://tutorialzine.com/2013/04/services-chooser-backbone-js/',
-// 	// 		title: 'Your First Backbone.js App – Service Chooser',
-// 	// 		image: 'http://cdn.tutorialzine.com/wp-content/uploads/2013/04/service_chooser_form-100x100.jpg'
-// 	// 	}
-// 	// ];
-
-// 	$scope.items = [
-// 		{
-// 			title: 'CS 107',
-// 			bldg: 'NVIDIA',
-// 			loc: '(1,2)'
-// 		},
-// 		{ 
-// 			title: 'CS 110',
-// 			bldg: 'CEMEX',
-// 			loc: '(3,4)'
-// 		},
-// 	];
-
-	// $scope.map = { center: { latitude: 45, longitude: -73 }, zoom: 8 };
-
-
-// }
