8e036a20a79741568feafd7bcd534dc91cebbf16
kmblake <kentblake@DN0a22b89a.SUNet>
2016-04-16 17:56:17 -0700

    Changed map to embedded

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 // Define a new module for our app
-var app = angular.module("classfinder", ['uiGmapgoogle-maps']);
+var app = angular.module("classfinder", ['uiGmapgoogle-maps']).config(function($sceDelegateProvider) {
+  $sceDelegateProvider.resourceUrlWhitelist([
+    // Allow same origin resource loads.
+    'self',
+    // Allow loading from our assets domain.  Notice the difference between * and **.
+    'https://www.google.com/maps/embed**'
+  ]);
+});
 
 // Create the instant search filter
 
@@ -39,29 +46,77 @@ app.controller('ClassfinderController', ['$scope', function($scope) {
   $scope.items = [
 		{
 			title: 'CS 107',
+			desc: 'Computer Systems',
 			bldg: 'NVIDIA',
-			location: {latitude: 37.4281111, longitude: -122.176272},
-			id: 1
-		},
+			loc: "https://www.google.com/maps/embed/v1/place?q=37%C2%B025'41.2%22N%20122%C2%B010'26.7%22W&key=AIzaSyAOpii1ESYwlE64cPlCCXRijnkymgNJp4A"
+		}, 
 		{ 
 			title: 'CS 110',
+			desc:'Principles of Computer Systems',
 			bldg: 'CEMEX',
-			location: {latitude: 37.4247218, longitude: -122.1649816},
-			id: 2
+			loc: "https://www.google.com/maps/embed/v1/place?q=37%C2%B025'41.2%22N%20122%C2%B010'26.7%22W&key=AIzaSyAOpii1ESYwlE64cPlCCXRijnkymgNJp4A"
+		}, 
+		{ 
+			title: 'CS 42',
+			desc: 'CallBack Me Maybe',
+			bldg: '530-127',
+			loc: "https://www.google.com/maps/embed/v1/place?q=37%C2%B025'34.5%22N%20122%C2%B010'14.9%22W&key=AIzaSyAOpii1ESYwlE64cPlCCXRijnkymgNJp4A"
+		},
+				{ 
+			title: 'CS 108',
+			desc: 'Object-Oriented Systems Design',
+			bldg: ' 370-370',
+			loc: "https://www.google.com/maps/embed/v1/place?q=37%C2%B025'40.7%22N%20122%C2%B010'17.6%22W&key=AIzaSyAOpii1ESYwlE64cPlCCXRijnkymgNJp4A"
+		},
+			{ 
+			title: 'CS 142',
+			desc: 'Web Applications',
+			bldg: ' 200-034',
+			loc: "https://www.google.com/maps/embed/v1/place?q=37%C2%B025'42.7%22N%20122%C2%B010'13.3%22W&key=AIzaSyAOpii1ESYwlE64cPlCCXRijnkymgNJp4A"
+		},
+		{ 
+			title: 'CS 143',
+			desc: 'Compilers',
+			bldg: 'NVIDIA Auditorium ',
+			loc: "https://www.google.com/maps/embed/v1/place?q=37%C2%B025'41.2%22N%20122%C2%B010'26.7%22W&key=AIzaSyAOpii1ESYwlE64cPlCCXRijnkymgNJp4A"
+		},
+		{ 
+			title: 'CS 145',
+			desc: ' Introduction to Databases',
+			bldg: 'NVIDIA Auditorium',
+			loc:  "https://www.google.com/maps/embed/v1/place?q=37%C2%B025'41.2%22N%20122%C2%B010'26.7%22W&key=AIzaSyAOpii1ESYwlE64cPlCCXRijnkymgNJp4A"
+		},
+		{ 
+			title: 'CS 148',
+			desc: 'Introduction to Computer Graphics and Imaging',
+			bldg: 'Gates B3 ',
+			loc: "https://www.google.com/maps/embed/v1/place?q=37%C2%B025'48.0%22N%20122%C2%B010'23.6%22W&key=AIzaSyAOpii1ESYwlE64cPlCCXRijnkymgNJp4A"
+		},
+		{ 
+			title: 'CS 193P',
+			desc: 'iPhone and iPad Application Programming',
+			bldg: 'Hewlett Teaching Center 200 ',
+			loc: "https://www.google.com/maps/embed/v1/place?q=37%C2%B025'44.9%22N%20122%C2%B010'22.3%22W&key=AIzaSyAOpii1ESYwlE64cPlCCXRijnkymgNJp4A"
+		},
+		{ 
+			title: 'CS 211',
+			desc: 'Content Creation in Virtual Reality',
+			bldg: 'Gates ',
+			loc: "https://www.google.com/maps/embed/v1/place?q=37%C2%B025'48.0%22N%20122%C2%B010'23.6%22W&key=AIzaSyAOpii1ESYwlE64cPlCCXRijnkymgNJp4A"
 		}
-
 	];
+}])
 
 	// $scope.map = { center: { latitude: 45, longitude: -73 }, zoom: 8 };
 	// $scope.map = {center: {latitude: 40.1451, longitude: -99.6680 }, zoom: 4 };
-  $scope.map = {center: {latitude: 37.4280888, longitude: -122.1723306 }, zoom: 16 };
-  $scope.options = {scrollwheel: false};
-  $scope.coordsUpdates = 0;
-  $scope.dynamicMoveCtr = 0;
-  $scope.marker = {
-    options: { draggable: false }
-  };
-}]);
+  // $scope.map = {center: {latitude: 37.4280888, longitude: -122.1723306 }, zoom: 16 };
+  // $scope.options = {scrollwheel: false};
+  // // $scope.coordsUpdates = 0;
+  // // $scope.dynamicMoveCtr = 0;
+  // $scope.marker = {
+  //   options: { draggable: false }
+  // };
+// }]);
 
 // function ClassfinderController($scope){
 
